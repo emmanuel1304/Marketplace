1aa724dc9d6806a3f3ad1a5574d25aaec9ff63a9
lucky okirie <okirielucky1304@gmail.com>
2023-03-14 11:39:59 -0500

    first commit

--- marketapp/models.py
+++ marketapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=255)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
