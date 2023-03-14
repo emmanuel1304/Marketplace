08458f2ff3042e34b740e41dbec06e9be4461f18
lucky okirie <okirielucky1304@gmail.com>
2023-03-14 13:41:37 -0500

    changes

--- marketapp/models.py
+++ marketapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Categories(models.Model):
@@ -8,5 +8,18 @@ class Categories(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
     
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Categories, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
